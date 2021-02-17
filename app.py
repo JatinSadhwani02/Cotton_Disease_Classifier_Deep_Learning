4cb6d5dfd04f346ea772c9c898f28620a95ab8b2
Jatin Sadhwani <75443345+JatinSadhwani02@users.noreply.github.com>
2021-02-17 16:25:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,21 +11,21 @@ from keras.models import load_model
 #load model
 model =load_model("predicted_cotton_disease_model.h5")
 
-print('@@ Model loaded')
+print('************Model loaded*************')
 
 
 def pred_cot_dieas(cott_plant):
   test_image = load_img(cott_plant, target_size = (150, 150)) # load image 
-  print("@@ Got Image for prediction")
+  print("***************Got Image for prediction*******************")
   
   test_image = img_to_array(test_image)/255 # convert image to np array and normalize
   test_image = np.expand_dims(test_image, axis = 0) # change dimention 3D to 4D
   
   result = model.predict(test_image).round(3) # predict diseased palnt or not
-  print('@@ Raw result = ', result)
+  print('********************Raw result = ', result)
   
   pred = np.argmax(result) # get the index of max value
-  print('@@ final predicted value = ',pred)
+  print('********************final predicted value = ',pred)
 
   return pred
 
@@ -56,12 +56,12 @@ def predict():
   if request.method == 'POST':
     file = request.files['image'] # fet input
     filename = file.filename        
-    print("@@ Input posted = ", filename)
+    print("******************Input posted = ", filename)
         
     file_path = os.path.join('static/user uploaded', filename)
     file.save(file_path)
 
-    print("@@ Predicting class......")
+    print("******************Predicting class......")
     pred = pred_cot_dieas(cott_plant=file_path)
 
     if pred == 0:
